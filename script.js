3583292cfb9591e8076b8a867186616fb72b92b3
Sumit Garade <sumitgarade165@gmail.com>
2025-04-06 19:00:22 +0530

    Add

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeMessage() {
+  const letter = document.getElementById("birthdayLetter");
+  letter.innerHTML = `
+    Hey [MAYURI JII]!<br><br>
+    You thought that was it? Surprise!<br><br>
+    You are not just amazing — you’re one of a kind. I hope your day is filled with joy, cakes, balloons, and all the people who love you.<br><br>
+    Let’s make this birthday unforgettable!<br><br>
+    Cheers to YOU!<br>
+    [SUMIT]
+  `;
+}
